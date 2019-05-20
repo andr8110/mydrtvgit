4c73774e84882bfc12b906b8c04508bd33a81686
Malene Bærtelsen <male3549@stud.kea.dk>
2019-05-20 12:11:44 +0200

    cookies

--- index.php
+++ index.php
@@ -287,6 +287,29 @@ require_once './components/navSide.php';
 <!-- PAGE CONTENT WRAPPER END -->
 </div>
 <!-- D FLEX WRAPPER END -->
+<!--Modal: modalCookie-->
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"  id="onload">
+
+   
+
+      <div class="modal-dialog modal-frame modal-top modal-notify modal-info" role="document">
+    <!--Content-->
+    <div class="modal-content">
+      <!--Body-->
+      <div class="modal-body">
+        <div class="row d-flex justify-content-center align-items-center">
+
+          <p class="pt-3 pr-2">We use cookies to improve your website experience</p>
+
+          <a type="button" class="btn btn-primary btnLearnMore">Learn more <i class="fas fa-book ml-1"></i></a>
+          <a type="button" class="btn btn-outline-primary waves-effect btnOK" data-dismiss="modal">Ok, thanks</a>
+        </div>
+      </div>
+    </div>
+    <!--/.Content-->
+  </div>
+</div>
+<!--Modal: modalCookie-->
 
 
 
---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(window).on('load',function(){
+    var delayMs = 1500; // delay in milliseconds
+
+    setTimeout(function(){
+        $('.bs-example-modal-lg').modal('show');
+    }, delayMs);
+});  
+
