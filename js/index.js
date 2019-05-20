924737ea0220d50ad836389e56605e70001fdc18
Malene Bærtelsen <male3549@stud.kea.dk>
2019-05-20 16:50:20 +0200

    jsjsjsjsjjsjsjsjsjs

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load',function(){
-    var delayMs = 1500; // delay in milliseconds
-
-    setTimeout(function(){
-        $('.bs-example-modal-lg').modal('show');
-    }, delayMs);
-});  
-
